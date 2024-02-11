364da38c51bd7b97ab3508fd1991884ddc682d02
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-02-11 14:17:19 -0600

    feat: form validated when client is added

---
+++ js/new-client.js
@@ -0,0 +1,32 @@
+(function () {
+    const form = document.querySelector('#form');
+
+    form.addEventListener('submit', validateForm);
+
+    function validateForm(e) {
+        e.preventDefault();
+
+        const name = document.querySelector('#name').value;
+        const email = document.querySelector('#email').value;
+        const phone = document.querySelector('#phone').value;
+        const company = document.querySelector('#company').value;
+
+        const objNewClient = {
+            name,
+            email,
+            phone,
+            company
+        }
+
+        if (!validateObject(objNewClient)) {
+            console.log('no');
+            return
+        }
+
+        console.log('yes');
+    }
+
+    function validateObject(object) {
+        return Object.values(object).every(property => property !== '');
+    }
+})();
